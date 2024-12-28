8f898f06b020ea5050d67b4491ba9454686c5cc3
Mike Sazonov <mikhailsazon@gmail.com>
2024-12-29 01:50:13 +0300

    Refactor model pk models.py

--- app/db/models.py
+++ app/db/models.py
@@ -5,8 +5,12 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.database import Base
 
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
 class Wallet(Base):
     __tablename__: str = "wallet"
 
-    id: Mapped[str] = mapped_column(String, primary_key=True, index=True, unique=True, default=str(uuid.uuid4()))
+    id: Mapped[str] = mapped_column(String, primary_key=True, default=generate_uuid)
     balance: Mapped[int] = mapped_column(Integer, nullable=False)
